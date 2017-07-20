5962d63de33ee8a08389f502535afe739773ff51
zx142489 <zx142489@alibaba-inc.com>
2017-07-20 13:33:58 +0800

    proxy

---
+++ src/main/java/com/zx/test/Test.java
@@ -0,0 +1,23 @@
+package com.zx.test;
+
+import com.zx.proxy.ProxyHandler;
+import com.zx.proxy.SortService;
+import com.zx.proxy.SortServiceImpl;
+
+import java.lang.reflect.Proxy;
+
+public class Test {
+    public static void main(String [] args)
+    {
+        int [] a = {1,3,6,4,5};
+        SortService s = (SortService) Proxy.newProxyInstance(SortService.class.getClassLoader(),
+                new Class[] {SortService.class},
+                new ProxyHandler(new SortServiceImpl()));
+        //必须是Interface.class
+        s.sort(a);
+        for(int i:a)
+        {
+            System.out.println(i);
+        }
+    }
+}
