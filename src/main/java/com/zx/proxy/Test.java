f6661f039597e03a5e03b294b6dffb004d3e71c6
zx142489 <zx142489@alibaba-inc.com>
2017-07-24 10:13:00 +0800

    SpringIOC

--- src/main/java/com/zx/proxy/Test.java
+++ src/main/java/com/zx/proxy/Test.java
@@ -3,13 +3,13 @@ package com.zx.proxy;
 import java.lang.reflect.Proxy;
 
 public class Test {
+    private SortService s1;
     public static void main(String [] args)
     {
         int [] a = {1,3,6,4,5};
         SortService s = (SortService) Proxy.newProxyInstance(SortService.class.getClassLoader(),
                 new Class[] {SortService.class},
                 new ProxyHandler(new SortServiceImpl()));
-        //必须是Interface.class
         s.sort(a);
         for(int i:a)
         {
