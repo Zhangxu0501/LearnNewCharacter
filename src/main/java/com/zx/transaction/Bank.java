5783b0fc8a2a84f803ca67331ee1f7a6d3932886
zx142489 <zx142489@alibaba-inc.com>
2017-08-13 10:57:25 +0800

    添加图片

---
+++ src/main/java/com/zx/transaction/Bank.java
@@ -0,0 +1,6 @@
+package com.zx.transaction;
+
+public class Bank {
+    private Long id;
+    private String user;
+ }
--- src/main/java/com/zx/transaction/Test.java
+++ src/main/java/com/zx/transaction/Test.java
@@ -1,4 +1,4 @@
-package com.zx.test;
+package com.zx.transaction;
 
 public class Test {
     public static void main(String [] args)
